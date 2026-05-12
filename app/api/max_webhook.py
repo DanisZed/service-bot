3c07423fa9c17f91875b140e98226540008e86c1
DanisZed <uralentrade@gmail.com>
2026-05-13 01:50:33 +0500

    восстановление запросов

--- app/api/max_webhook.py
+++ app/api/max_webhook.py
@@ -5,7 +5,7 @@ from typing import Any, Dict
 from fastapi import APIRouter, Request, BackgroundTasks
 
 from max_client import MaxClient  # поправь путь, если файл лежит в другом месте
-from app.services.dialog_service import dialog_service  # твой модуль диалогов
+from app.services.old_dialog_service import dialog_service # твой модуль диалогов
 
 router = APIRouter()
 
