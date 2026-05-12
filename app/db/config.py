db1b9102a808953655ca4cc6f5e8864c242358d8
DanisZed <uralentrade@gmail.com>
2026-05-13 00:16:25 +0500

    Исправлены логин и пароль к бд

--- app/db/config.py
+++ app/db/config.py
@@ -2,5 +2,5 @@ import os
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql+asyncpg://postgres@localhost:5432/servicebot",
+    "postgresql+asyncpg://servicebot_app:D1a9n8i0s@localhost:5432/servicebot",
 )
