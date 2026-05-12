dcd2ff8dfebd31c78308bdbf17f9ef68913616d1
DanisZed <uralentrade@gmail.com>
2026-05-12 23:59:45 +0500

    смена логина к бд

--- app/db/config.py
+++ app/db/config.py
@@ -1,8 +1,6 @@
 import os
 
-# DATABASE_URL вида:
-# postgresql+asyncpg://user:password@localhost:5432/servicebot
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql+asyncpg://Danis@localhost:5432/servicebot",
+    "postgresql+asyncpg://postgres@localhost:5432/servicebot",
 )
