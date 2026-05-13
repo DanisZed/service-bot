ad78fc40d0b84baf097670f0e2880c752bc8ec6b
DanisZed <uralentrade@gmail.com>
2026-05-13 22:05:03 +0500

    удален мастер панель

--- app/main.py
+++ app/main.py
@@ -3,11 +3,9 @@ from fastapi import FastAPI
 from app.api.max_webhook import router as max_router
 from app.api.master_auth import router as master_auth_router
 from app.api.requests import router as requests_router
-from app.api import master_panel
 
 app = FastAPI()
 
 app.include_router(max_router)
 app.include_router(master_auth_router)
 app.include_router(requests_router)
-app.include_router(master_panel.router)
