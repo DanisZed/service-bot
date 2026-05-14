05a4e748252d8a66c9d16f1657ba85d7ad768142
DanisZed <uralentrade@gmail.com>
2026-05-15 02:08:54 +0500

    http

--- app/main.py
+++ app/main.py
@@ -32,6 +32,10 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://panel.daniszed.keenetic.pro",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://192.168.2.122:5173",
+    "http://192.168.2.122:8000",
 ]
 
 app.add_middleware(
