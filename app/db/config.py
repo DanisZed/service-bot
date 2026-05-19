9933a90afc977ec552c678fe4ac22412d375d753
DanisZed <uralentrade@gmail.com>
2026-05-19 15:04:35 +0500

    восстанавливаем первую миграцию

--- app/db/config.py
+++ app/db/config.py
@@ -1,6 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql+asyncpg://servicebot_app:D1a9n8i0s@localhost:5432/servicebot",
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
